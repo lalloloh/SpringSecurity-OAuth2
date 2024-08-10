a448eab4ea06b2517c7fc0d5c305b70ba933a130
Lairon Oliveira <laironoliveira@gmail.com>
2024-08-10 00:01:09 -0300

    feat: Implementado configuracao do SecurityFilterChain

---
+++ auth-server/src/main/java/br/com/laironoliveira/auth_server/SecurityFilterConfig.java
@@ -0,0 +1,42 @@
+package br.com.laironoliveira.auth_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+@Configuration
+public class SecurityFilterConfig {
+    
+    @Bean
+    @Order(1)
+    SecurityFilterChain authServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        
+        httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+            .oidc(Customizer.withDefaults());
+
+        httpSecurity.exceptionHandling((exceptionHandling) -> 
+            exceptionHandling.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))    
+        )
+        .oauth2ResourceServer((oauth2ResourceServer) -> 
+            oauth2ResourceServer.jwt(Customizer.withDefaults())
+        );
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain defauSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests((authorize) -> 
+            authorize.anyRequest().authenticated()
+        )
+        .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
---
+++ auth-server/src/test/java/br/com/laironoliveira/auth_server/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.laironoliveira.auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
