c39da68663bccd38ee80760f8e6acfaa4eca7e66
Lairon Oliveira <laironoliveira@gmail.com>
2024-08-10 18:26:10 -0300

    fix: aplicacao de definicoes padrao para o httpsecurity

--- auth-server/src/main/java/br/com/laironoliveira/auth_server/Configuration/SecurityFilterConfig.java
+++ auth-server/src/main/java/br/com/laironoliveira/auth_server/Configuration/SecurityFilterConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -16,7 +17,8 @@ public class SecurityFilterConfig {
     @Order(1)
     SecurityFilterChain authServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
         
-        //TODO: Verificar instância nula do configurer
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+
         httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
             .oidc(Customizer.withDefaults());
 
